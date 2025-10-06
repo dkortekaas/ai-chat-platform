9c0c8cfd0542e7760613e9d74a321368efc05667
Dennis Kortekaas <d.kortekaas@icloud.com>
2025-10-06 23:53:47 +0200

    fix

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -29,44 +29,7 @@ export default function SignupPage() {
           <div className="flex justify-center">
             <div className="relative">
               {/* Person illustration */}
-              <div className="w-80 h-80 relative">
-                {/* Person figure */}
-                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
-                  {/* Head */}
-                  <div className="w-16 h-16 bg-gray-200 rounded-full mb-2 relative">
-                    {/* Hair braid */}
-                    <div className="absolute -right-2 top-2 w-8 h-12 bg-gray-800 rounded-full transform rotate-12"></div>
-                  </div>
-                  
-                  {/* Body */}
-                  <div className="w-20 h-24 bg-indigo-500 rounded-t-lg mb-2"></div>
-                  
-                  {/* Arms */}
-                  <div className="absolute -left-4 top-8 w-6 h-16 bg-gray-200 rounded-full transform -rotate-12"></div>
-                  <div className="absolute -right-4 top-8 w-6 h-16 bg-gray-200 rounded-full transform rotate-12"></div>
-                  
-                  {/* Legs */}
-                  <div className="w-8 h-16 bg-gray-200 rounded-b-lg inline-block mr-2"></div>
-                  <div className="w-8 h-16 bg-gray-200 rounded-b-lg inline-block"></div>
-                </div>
-                
-                {/* Desk */}
-                <div className="absolute bottom-0 left-0 right-0 h-8 bg-white rounded-lg shadow-lg">
-                  {/* Tablet */}
-                  <div className="absolute left-8 top-2 w-12 h-8 bg-white border-2 border-gray-300 rounded">
-                    <div className="w-4 h-4 bg-green-500 rounded-full mx-auto mt-1"></div>
-                  </div>
-                  
-                  {/* Monitor */}
-                  <div className="absolute right-8 top-1 w-16 h-6 bg-black rounded">
-                    <div className="w-6 h-4 bg-green-500 rounded mx-auto mt-1"></div>
-                    <div className="text-white text-xs text-center mt-1">✓ ✓ ✓</div>
-                  </div>
-                  
-                  {/* Coffee cup */}
-                  <div className="absolute left-1/2 top-1 w-3 h-4 bg-white border border-gray-300 rounded-sm"></div>
-                </div>
-              </div>
+
             </div>
           </div>
         </div>
